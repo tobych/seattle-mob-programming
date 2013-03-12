b34cc2b64056cfcca9683009b0c0deaebd458f08
Innovation Lab <innovations-lab@googlegroups.com>
2013-03-12 10:51:16 -0700

    Session 3 Node/Socket Chat Session three ended with a basic working Node/Express/Socket.io chat app It's worth noting it is a naive implementation and has a simple injection vulnerability as well as no tests of any kind.

---
+++ sessions/session_3/app.js
@@ -0,0 +1,50 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+
+io.sockets.on('connection', function (socket) {
+  socket.on('message', function (data) {
+    socket.broadcast.send(data)
+    socket.send(data)
+    console.log(data)
+  });
+});
+
+
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+server.listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
